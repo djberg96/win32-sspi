aa2c8e3d01c1c9e493c9a2dd7deccae64f1b5b45
Daniel Berger <djberg96@gmail.com>
2012-07-27 14:59:14 -0600

    Added some more tests.

--- test/test_win32_sspi_client.rb
+++ test/test_win32_sspi_client.rb
@@ -3,10 +3,13 @@
 ########################################################################
 require 'test-unit'
 require 'win32/sspi/client'
+require 'win32/sspi/server'
 
 class TC_Win32_SSPI_Client < Test::Unit::TestCase
   def setup
     @client = Win32::SSPI::Client.new
+    @server = Win32::SSPI::Client.new
+    @type1 = nil
   end
 
   test "username basic functionality" do
@@ -66,7 +69,30 @@ class TC_Win32_SSPI_Client < Test::Unit::TestCase
     assert_nil(@client.type_3_message)
   end
 
+  test "initial_token basic functionality" do
+    assert_respond_to(@client, :initial_token)
+  end
+
+  test "initial_token method accepts an argument" do
+    assert_nothing_raised{ @client.initial_token(true) }
+  end
+
+  test "initial_token generates and returns an expected token" do
+    assert_nothing_raised{ @type1 = @client.initial_token }
+    assert_kind_of(String, @type1)
+    assert_true(@type1.size > 10)
+  end
+
+  test "the type_1_message accessor is set after initial_token is called" do
+    @client.initial_token
+    assert_not_nil(@client.type_1_message)
+  end
+
+  test "complete authentication basic functionality" do
+  end
+
   def teardown
     @client = nil
+    @type1 = nil
   end
 end
